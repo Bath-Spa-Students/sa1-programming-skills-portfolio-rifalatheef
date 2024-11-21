08664f6408935c0f7ab059e3606c28865a2a11d3
rifalatheef <rifalatheef142004@gmail.com>
2024-11-21 14:41:22 +0400

    some counting.py

---
+++ Exercise 07/Some counting.py
@@ -0,0 +1,38 @@
+"""Use your newly acquired knowledge of the for loop to complete the following
+tasks. Print all values to the console in each case. * Write a loop that counts
+up from 0 to 50 in increments of 1. * Write a loop that counts down from 50 to
+0 in decrements of 1. * Write a loop that counts up from 30 to 50 in increments
+of 1. * Write a loop that counts down from 50 to 10 in decrements of 2. * Write
+a loop that counts up from 100 to 200 in increments of 5. You may include all
+loops in a single project"""
+
+# Loop that counts up from 0 to 50 in increments of 1
+print("Counting up from 0 to 50 in increments by 1:")
+for i in range(0, 51, 1):
+    print(i, end=" ")
+print("\n")
+
+# Loop that counts down from 50 to 0 by reduces by  1
+print("Counting down from 50 to 0 by reduction 1:")
+for i in range(50, -1, -1):
+    print(i, end=" ")
+print("\n")
+
+# Loop that counts up from 30 to 50 added by  1
+print("Counting up from 30 to 50 addition of 1:")
+for i in range(30, 51, 1):
+    print(i, end=" ")
+print("\n")
+
+# Loop that counts down from 50 to 10 by decreases of 2
+print("Counting down from 50 to 10 by decreases  of 2:")
+for i in range(50, 9, -2):
+    print(i, end=" ")
+print("\n")
+
+# Loop that counts up from 100 to 200 in increments of 5
+print("Counting up from 100 to 200 in increments of 5:")
+for i in range(100, 201, 5):
+    print(i, end=" ")
+print("\n")
+
