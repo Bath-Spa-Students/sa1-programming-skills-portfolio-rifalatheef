6ec802aa68d07746229f146c77c54bd5120bd488
rifalatheef <rifalatheef142004@gmail.com>
2024-11-22 14:59:03 +0400

    Functions -Favorite Book.py

---
+++ z_bonus_exercise/Functions -Favorite Book.py
@@ -0,0 +1,6 @@
+# Define the function
+def favorite_book(title):
+    print(f"One of my favorite books is {title}.")
+
+# Call the function with a book title as an argument
+favorite_book("Wings of Fire")
