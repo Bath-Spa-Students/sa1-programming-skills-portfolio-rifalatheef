f9544956d46d9f71bf5417430e813b2be2869e26
rifalatheef <rifalatheef142004@gmail.com>
2024-11-22 14:49:56 +0400

    Strings Concatenation.py

---
+++ z_bonus_exercise/Strings Concatenation.py
@@ -0,0 +1,10 @@
+"""Write three strings in different variables and print the output as one string."""
+
+# Store three strings in different variables
+string1 = "studying"
+string2 = "Python"
+string3 = "is cool!"
+
+# print the output as one string
+combined_string = string1 + " " + string2 + " " + string3
+print(combined_string)
