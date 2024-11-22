88e35f145d35462fcbdcf1337a29fd968f8a225f
rifalatheef <rifalatheef142004@gmail.com>
2024-11-22 14:55:37 +0400

    Control Structures.py

---
+++ z_bonus_exercise/Control Structures.py
@@ -0,0 +1,14 @@
+# Assigning the a color
+alien_color = 'green'
+
+# verify if the color is green
+if alien_color == 'green':
+    print("The player just earned 5 points!")
+
+# Assigning the color
+alien_color = 'red'
+
+# verify if the alien color is red
+
+if alien_color == 'green':
+    print("The player just earned 5 points!")
