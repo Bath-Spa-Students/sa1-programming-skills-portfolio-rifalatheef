2b6a28e9781623b3a1474e5963699a4369c04049
rifalatheef <rifalatheef142004@gmail.com>
2024-11-22 14:54:14 +0400

    List -Names.py

---
+++ z_bonus_exercise/List -Names.py
@@ -0,0 +1,8 @@
+# list the names of a few friends
+names = ["Huda", "Anaswara", "Haifa", "sivakami"]
+
+# Print each person's name 
+print(names[0])
+print(names[1])
+print(names[2])
+print(names[3])
