b416fce79d73aec50d81f5f0f01b25c9f721adc1
rifalatheef <rifalatheef142004@gmail.com>
2024-11-22 14:56:47 +0400

    Dictionaries.py

---
+++ z_bonus_exercise/Dictionaries.py
@@ -0,0 +1,13 @@
+# make a dictionary
+person = {
+    "first_name": "marry",
+    "last_name": "francis",
+    "age": 40,
+    "city": "france"
+}
+
+# Print each piece of information in seperate lines
+print(f"First Name: {person['first_name']}")
+print(f"Last Name: {person['last_name']}")
+print(f"Age: {person['age']}")
+print(f"City: {person['city']}")
