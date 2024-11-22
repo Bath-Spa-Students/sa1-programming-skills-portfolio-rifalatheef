81aa36e69b6ae889f7afd8ac9c815d89bc57e383
rifalatheef <rifalatheef142004@gmail.com>
2024-11-22 14:53:18 +0400

    Variables (Favorite Number).py

---
+++ z_bonus_exercise/Variables (Favorite Number).py
@@ -0,0 +1,8 @@
+#create your favorite number
+favorite_number = 9
+
+# Create a message addressing  your favorite number
+message = f"My favorite number is {favorite_number}."
+
+# Print the message
+print(message)
